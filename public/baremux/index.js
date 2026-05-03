13d9435be897db0ffb144baa50853da7e75d77cc
david <orbinxproductions@gmail.com>
2026-05-02 22:40:13 -0400

    fix setting sagian

--- public/baremux/index.js
+++
@@ -1,9 +0,0 @@
-import * as BareMuxModule from "./index.mjs?v=3";
-
-if (typeof globalThis !== 'undefined') {
-  globalThis.BareMux = BareMuxModule;
-  globalThis.BareMuxConnection = BareMuxModule.BareMuxConnection; // Defensive direct export
-}
-
-export * from "./index.mjs?v=3";
-export default BareMuxModule.default;
